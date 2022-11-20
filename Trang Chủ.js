4a03a0102f778fb9c33c5eb26691155e4caf4626
b3tran <tranthybinh@gmail.com>
2022-11-20 12:58:57 +0700

    Slide Auto

---
+++ Trang Chủ.js
@@ -0,0 +1,49 @@
+/* Slide image auto */
+var slideIndex = 1;
+changeImage();
+showDivs(slideIndex);
+
+function plusDivs(n) {
+showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+var i;
+var x = document.getElementsByClassName("slides");
+var dots = document.getElementsByClassName("image-badge");
+if (n > x.length) {slideIndex = 1}
+if (n < 1) {slideIndex = x.length}
+for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+}
+for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" badge-white", "");
+}
+x[slideIndex-1].style.display = "block";  
+dots[slideIndex-1].className += " badge-white";
+}
+
+function changeImage(){
+    var i;
+    var x = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("image-badge");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > x.length) 
+    {
+        slideIndex = 1
+    }    
+    x[slideIndex-1].style.display = "block";  
+    for (i = 0; i < dots.length; i++) 
+    {
+        dots[i].className = dots[i].className.replace(" badge-white", "");
+    }
+    dots[slideIndex-1].className += " badge-white"; 
+    setTimeout(changeImage, 11000);
+}
