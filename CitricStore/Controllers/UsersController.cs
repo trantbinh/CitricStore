f28d860c8d2c8964100f180c800b987f33463aed
NguyenCanhPhong <Nguyencanhphong246@gmail.com>
2022-11-22 19:05:38 +0700

    làm thanh phân trang cho trang lịch sử mua hàng

--- CitricStore/Controllers/UsersController.cs
+++ CitricStore/Controllers/UsersController.cs
@@ -7,6 +7,7 @@ using System.Web;
 using System.Web.Mvc;
 using CitricStore.Models;
 using System.Resources;
+using PagedList;
 
 namespace CitricStore.Controllers
 {
@@ -241,6 +242,32 @@ namespace CitricStore.Controllers
 
 
         //Đơn đã mua
+
+        public ActionResult Index(int? page)
+        {
+
+            // 1. Tham số int? dùng để thể hiện null và kiểu int
+            // page có thể có giá trị là null và kiểu int.
+
+            // 2. Nếu page = null thì đặt lại là 1.
+            if (page == null) page = 1;
+
+            // 3. Tạo truy vấn, lưu ý phải sắp xếp theo trường nào đó, ví dụ OrderBy
+            // theo LinkID mới có thể phân trang.
+            var links = (from l in database.ORDER_INFO
+                         select l).OrderBy(x => x.MaOrder);
+
+            // 4. Tạo kích thước trang (pageSize) hay là số Link hiển thị trên 1 trang
+            int pageSize = 3;
+
+            // 4.1 Toán tử ?? trong C# mô tả nếu page khác null thì lấy giá trị page, còn
+            // nếu page = null thì lấy giá trị 1 cho biến pageNumber.
+            int pageNumber = (page ?? 1);
+
+            // 5. Trả về các Link được phân trang theo kích thước và số trang.
+            return View(links.ToPagedList(pageNumber, pageSize));
+        }
+
         public ActionResult Users_DaMua(int idkh)
         {
             var info = database.ORDER_INFO.Where(s => s.MaKH == idkh).ToList();
@@ -258,6 +285,8 @@ namespace CitricStore.Controllers
             var ud = database.OVERALLs.Where(s => s.Ma == idud) ;
             return PartialView(ud);
         }
+
+
 
         //Game đã có
     }
